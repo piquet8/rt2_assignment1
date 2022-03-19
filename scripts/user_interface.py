3a2c57f9fea4f4f9bb03ac6fa8503f6bed37c173
Gianluca Piquet <73198462+piquet8@users.noreply.github.com>
2022-03-19 23:04:21 +0000

    Add files via upload

---
+++ scripts/user_interface.py
@@ -0,0 +1,32 @@
+import rospy
+import time
+from rt2_assignment1.srv import Command
+import actionlib
+import rt2_assignment1.msg	
+from geometry_msgs.msg import Twist
+
+def main():
+    rospy.init_node('user_interface')
+    ui_client = rospy.ServiceProxy('/user_interface', Command)
+    action_client = actionlib.SimpleActionClient('go_to_point', rt2_assignment1.msg.ReachGoalAction)
+    pub_ = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+    time.sleep(10)
+    rate = rospy.Rate(20)
+    x = int(input("\nPress 1 to start the robot "))
+    while not rospy.is_shutdown():
+        if (x == 1):
+            ui_client("start")
+            x = int(input("\nPress 0 to stop the robot "))
+        else:
+            action_client.cancel_all_goals()
+            twist_msg = Twist()
+            twist_msg.linear.x = 0	
+            twist_msg.linear.y = 0
+            twist_msg.angular.z = 0
+            pub_.publish(twist_msg)
+            
+            ui_client("stop")
+            x = int(input("\nPress 1 to start the robot "))
+            
+if __name__ == '__main__':
+    main()
---
+++ src/state_machine.cpp
@@ -0,0 +1,63 @@
+#include "ros/ros.h"
+#include "rt2_assignment1/Command.h"
+#include "rt2_assignment1/RandomPosition.h"
+
+#include <actionlib/client/simple_action_client.h>
+#include <actionlib/client/terminal_state.h>
+
+#include <rt2_assignment1/ReachGoalAction.h>
+
+
+bool start = false;
+
+bool user_interface(rt2_assignment1::Command::Request &req, rt2_assignment1::Command::Response &res){
+    if (req.command == "start"){
+    	start = true;
+    }
+    else {
+    	start = false;
+    }
+    return true;
+}
+
+
+int main(int argc, char **argv)
+{
+   ros::init(argc, argv, "state_machine");
+   ros::NodeHandle n;
+   ros::ServiceServer service= n.advertiseService("/user_interface", user_interface);
+   ros::ServiceClient client_rp = n.serviceClient<rt2_assignment1::RandomPosition>("/position_server");
+   actionlib::SimpleActionClient<rt2_assignment1::ReachGoalAction> ac("go_to_point", true);
+   rt2_assignment1::RandomPosition rp;
+   rt2_assignment1::ReachGoalGoal goal;
+   
+   rp.request.x_max = 5.0;
+   rp.request.x_min = -5.0;
+   rp.request.y_max = 5.0;
+   rp.request.y_min = -5.0;
+   
+      
+   while(ros::ok()){
+   	ros::spinOnce();
+   	if (start){
+   		client_rp.call(rp);
+   		ROS_INFO("Waiting for action server to start..");
+   		ac.waitForServer();
+   		
+   		goal.x = rp.response.x;
+   		goal.y = rp.response.y;
+   		goal.theta = rp.response.theta;
+   		std::cout << "\nGoing to the position: x= " << goal.x << " y= " << goal.y << " theta = " << goal.theta << std::endl;
+   	ac.sendGoal(goal);
+   		bool finished_before_timeout = ac.waitForResult(ros::Duration(120.0));
+   		
+   		if (finished_before_timeout)
+   		{
+   			ROS_INFO("Position Reached");
+   		}
+   		else
+   			ROS_INFO("Action didn't finish before the time out");
+   	}
+   }
+   return 0;
+}
